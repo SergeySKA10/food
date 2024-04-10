aec81ec83c077273fd14afd679ccf9194e62b5cd
Sergey <karakoskinsergej@gmail.com>
2024-04-10 21:37:11 +0300

    added timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,9 +7,14 @@ var __webpack_exports__ = {};
 
 
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
+
   const tabsHeader = document.querySelector('.tabheader__items'),
     tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent');
+
+  //Функция скрытия контента
+
   function hiddeTabContent(els, contents) {
     els.forEach(tab => {
       tab.classList.remove('tabheader__item_active');
@@ -19,6 +24,9 @@ window.addEventListener('DOMContentLoaded', () => {
       item.classList.remove('show', 'fade');
     });
   }
+
+  //Функция показа контента
+
   function showTabContent(els, contents, i = 0) {
     els[i].classList.add('tabheader__item_active');
     contents[i].classList.add('show', 'fade');
@@ -26,6 +34,9 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   hiddeTabContent(tabs, tabsContent);
   showTabContent(tabs, tabsContent);
+
+  //Обработчик на табы
+
   tabsHeader.addEventListener('click', e => {
     if (e.target && e.target.matches('.tabheader__item')) {
       tabs.forEach((tab, ind) => {
@@ -36,6 +47,64 @@ window.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+
+  //Timer
+
+  //const deadline = Date.parse(new Date()) + 60000;  // реализовать функционал записи deadline в localstrage (индивидуальный таймер для каждого пользователя)
+
+  const time = '2024-05-20',
+    // дата окончания акции
+    deadline = Date.parse(time);
+
+  //Функция добавления 0 для значений в таймере которые < 10
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  //Функция получения временных значений до окончания акции 
+
+  function getTimeRemaining(endtime) {
+    const total = endtime - new Date().getTime(),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours = Math.floor(total / (1000 * 60 * 60) % 24),
+      minutes = Math.floor(total / (1000 * 60) % 60),
+      seconds = Math.floor(total / 1000 % 60);
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+
+  //Функция установки часов и внутренняя функция обновления часов с их остановкой
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.textContent = getZero(t.days);
+      hours.textContent = getZero(t.hours);
+      minutes.textContent = getZero(t.minutes);
+      seconds.textContent = getZero(t.seconds);
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock('.timer', deadline);
 });
 /******/ })()
 ;
